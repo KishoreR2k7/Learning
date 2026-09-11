ae9976a49020b210b3faa0003eb6d544e0711f8f
KishoreR2k7 <kishorerajaji65@gmail.com>
2026-09-11 21:44:37 +0530

    feat: add solution for Codeforces 231A Team problem

---
+++ DSA-Learning/codeforces-probelms-solve/231A-Team/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < 3; j++) {
+                sum += scan.nextInt();
+            }
+            if (sum >= 2) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
